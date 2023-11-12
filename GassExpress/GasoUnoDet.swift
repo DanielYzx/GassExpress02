0c425aeb40c01654338768850200744f4d0037c8
daniel <2525102018@mail.utec.edu.sv>
2023-11-12 10:06:08 -0600

    actualizada

--- GassExpress/GasoUnoDet.swift
+++ GassExpress/GasoUnoDet.swift
@@ -9,7 +9,103 @@ import SwiftUI
 
 struct GasoUnoDet: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Cambia el color de fondo a tu preferencia
+        Color("gris")
+            .edgesIgnoringSafeArea(.all) // Ignora los límites seguros para que el color de fondo se extienda por toda la pantalla
+            
+            .overlay(
+                VStack {
+                    Text("Gasolinera: Uno la Gloria")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.black) // Cambia el color del texto si es necesario
+                    
+                    Image("gasuno")
+                        .resizable()
+                        .frame(width: 150, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .padding()
+                    
+                    Group{
+                        Text("Ubicacion:")
+                            
+                        Text("1era Avenida Norte ")
+                            
+                        
+                        Divider()
+                            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                            .padding()
+                        VStack{
+                            HStack{
+                                Text("Tipo:")
+                                    
+                                    .padding(.trailing)
+                      
+                                
+                                Text("Precio:")
+                                    
+                                    .padding(.leading)
+                                
+                                
+                            }
+                            
+                            HStack{
+                                Text("Diesel:  ")
+                                   
+                                    .padding(.trailing)
+                      
+                                
+                                Text("$ 4.40")
+                                    
+                                    .padding(.leading)
+                                
+                                
+                            }
+                            HStack{
+                                Text("Regular:")
+                                   
+                                    .padding(.trailing)
+                      
+                                
+                                Text("$ 3.48")
+                                    
+                                    .padding(.leading)
+                                
+                                
+                            }
+                            HStack{
+                                Text("Especial:")
+                                   
+                                    .padding(.trailing)
+                      
+                                
+                                Text("$ 5.50")
+                                    
+                                    .padding(.leading)
+                                
+                                
+                            }
+                        }
+                        
+                    }
+                    
+                    Button("Aceptar") {
+                      
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .padding(10)
+                    .background(Color(hex: "#64CCC5"))
+                    .font(.system(size: 18))
+                    .cornerRadius(6)
+                    .foregroundColor(.white)
+                    .font(.headline)
+                    .padding(10)
+                    .shadow(radius: 10 )
+
+                    
+                    Spacer()
+                    
+                }
+                
+            )
     }
 }
 
@@ -18,3 +114,4 @@ struct GasoUnoDet_Previews: PreviewProvider {
         GasoUnoDet()
     }
 }
+
